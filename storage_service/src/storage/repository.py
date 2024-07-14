0598f267543f1eb2182d72751e77cf1e34c14d37
Arkhip McNutt <ortiz47@mail.ru>
2024-07-14 12:54:41 +0400

    minor fix

--- storage_service/src/storage/repository.py
+++ storage_service/src/storage/repository.py
@@ -31,6 +31,8 @@ class Repository(AbstractCRUDRepository):
                 query = select(Meme).where(Meme.id == id_)
                 result = await session.execute(query)
                 memes = list(result.scalars().all())
+                if len(memes) == 0 and id_ is not None:
+                    raise DatabaseMemeNotFoundException()
             except SQLAlchemyError as exc:
                 logger.error(f'{type(exc)} raised {exc.args[0]}')
                 raise InternalRepositoryException()
